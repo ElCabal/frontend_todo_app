dc52e8e70437dd5d85e0d377181fe7dd874d4ad5
ElCabal <cabalholguinlufe@gmail.com>
2022-04-06 13:21:39 -0500

    littles changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Title from "./Components/Title";
 
 function App() {
   return (
-    <div className=" bg-slate-100 h-screen flex flex-col gap-5 items-center justify-between">
+    <div className=" bg-slate-100 h-screen flex flex-col gap-5 items-center justify-between py-10">
       <Title />
       <Container />
       <Footer />
